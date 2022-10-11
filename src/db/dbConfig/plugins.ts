51056f110920cbd535e1f32e6ee2af0b1bacfb59
mwibutsa <mflohost@gmail.com>
2022-10-11 08:51:07 +0200

    Initial project setup

---
+++ src/db/dbConfig/plugins.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema, Document } from 'mongoose';
+/* eslint-disable no-param-reassign */
+export const findOrCreatePlugin = function (schema: Schema): void {
+  schema.statics.findOrCreate = async function (condition, doc = null) {
+    const res = await this.findOne(condition);
+    return res || this.create(doc ?? condition);
+  };
+};
+
+export const formatJSONPlugin = function (schema: Schema): void {
+  schema.options.toJSON = {
+    transform(_doc: Document, ret: Document): Document {
+      ret.id = ret._id;
+      delete ret._id;
+      delete ret.__v;
+
+      if ('password' in ret) {
+        delete ret?.password;
+      }
+      return ret;
+    },
+  };
+};
+
+mongoose.plugin(findOrCreatePlugin);
+mongoose.plugin(formatJSONPlugin);
